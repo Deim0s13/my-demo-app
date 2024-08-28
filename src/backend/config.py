78acd445d26ad114d33584733ce1e79de5bb0ba3
Deim0s13 <leathen@gmx.de>
2024-08-28 12:40:08 +1200

    Changes to config.py to allow testing of the native app to function correctly

--- src/backend/config.py
+++ src/backend/config.py
@@ -2,5 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://mydemoappuser:password@localhost/mydemoapp_db'
+
+    # Adjust database URL depending on the environment
+    if os.environ.get('FLASK_ENV') == 'production':
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://mydemoappuser:password@db/mydemoapp_db'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://mydemoappuser:password@localhost/mydemoapp_db'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
